33d32f2c7e3202d3060c06c0d569f6ae9016e2ab
ncaco <ncaco97@gmail.com>
2025-03-22 21:27:57 +0900

    portal

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
-
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+
+  /* config options here */
+
+  images: {
+    domains: ['placehold.co'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+
+};
+
+module.exports = nextConfig;
+
+
+
